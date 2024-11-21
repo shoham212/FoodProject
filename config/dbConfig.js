ae57eaf698d0a6add931dcf718b757f643f2593e
SHOHAM212 <shoamhadar@gmail.com>
2024-11-21 19:32:40 +0200

    Added all files and updates

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,9 @@
 
 const dbConfig = {
-    user: 'shohamshervi_SQLLogin_1',         // שם המשתמש שקיבלת מ-Somee.com
-    password: 'ewrrkwucj1',     // הסיסמה שקיבלת מ-Somee.com
-    server: '	foodproject.mssql.somee.com', // כתובת השרת של מסד הנתונים מ-Somee.com
-    database: 'foodproject', // שם מסד הנתונים
+    user: process.env.DB_USER, // שם המשתמש לבסיס הנתונים
+    password: process.env.DB_PASSWORD, // סיסמת בסיס הנתונים
+    server: process.env.DB_SERVER, // כתובת השרת (למשל somedb.somee.com)
+    database: process.env.DB_NAME, // שם בסיס הנתונים
     options: {
         encrypt: true,              // הגדרות אבטחה, מומלץ להשאיר כך
         trustServerCertificate: true, // הוסיפי את השורה הזו
@@ -11,9 +11,16 @@ const dbConfig = {
     }
 };
 
-
-module.exports = {
-    dbConfig
-};
- 
-
+// פונקציה לחיבור לבסיס הנתונים
+const connectToDatabase = async () => {
+    try {
+      const pool = await sql.connect(dbConfig);
+      console.log('Connected to SQL Server');
+      return pool; // מחזירה את החיבור
+    } catch (error) {
+      console.error('Database connection failed:', error);
+      throw error; // זורקת שגיאה אם החיבור נכשל
+    }
+  };
+  
+  module.exports = { sql, connectToDatabase };
